628d2e79a1a55c064ed6ad7f1fc08d6f9c3944b8
kyrlang <kyrla.nunes@gmail.com>
2018-06-12 15:35:18 -0300

    bkp

---
+++ ArquivoXML/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ArquivoXML
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.ofd1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grdXml = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblArquivo = new System.Windows.Forms.Label();
+            this.btnXml = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdXml)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ofd1
+            // 
+            this.ofd1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.grdXml);
+            this.groupBox1.Location = new System.Drawing.Point(13, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(830, 389);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arquivo XML";
+            // 
+            // grdXml
+            // 
+            this.grdXml.AllowUserToOrderColumns = true;
+            this.grdXml.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdXml.Location = new System.Drawing.Point(13, 25);
+            this.grdXml.Name = "grdXml";
+            this.grdXml.Size = new System.Drawing.Size(800, 346);
+            this.grdXml.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblArquivo);
+            this.groupBox2.Controls.Add(this.btnXml);
+            this.groupBox2.Location = new System.Drawing.Point(13, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(827, 78);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // lblArquivo
+            // 
+            this.lblArquivo.AutoSize = true;
+            this.lblArquivo.Location = new System.Drawing.Point(213, 36);
+            this.lblArquivo.Name = "lblArquivo";
+            this.lblArquivo.Size = new System.Drawing.Size(87, 13);
+            this.lblArquivo.TabIndex = 5;
+            this.lblArquivo.Text = "Arquivo XML.:";
+            this.lblArquivo.Visible = false;
+            // 
+            // btnXml
+            // 
+            this.btnXml.AutoSize = true;
+            this.btnXml.Image = global::ArquivoXML.Properties.Resources._1350779123_xml4;
+            this.btnXml.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnXml.Location = new System.Drawing.Point(8, 23);
+            this.btnXml.Name = "btnXml";
+            this.btnXml.Size = new System.Drawing.Size(187, 38);
+            this.btnXml.TabIndex = 4;
+            this.btnXml.Text = "Selecionar Arquivo XML";
+            this.btnXml.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnXml.UseVisualStyleBackColor = true;
+            this.btnXml.Click += new System.EventHandler(this.btnXml_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 510);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GROUP NOTA FISCAL";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdXml)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofd1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView grdXml;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblArquivo;
+        private System.Windows.Forms.Button btnXml;
+    }
+}
+
---
+++ ArquivoXML/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ArquivoXML
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnXml_Click(object sender, EventArgs e)
+        {
+                      //define as propriedades do controle 
+            //OpenFileDialog
+            //this.ofd1.Multiselect = true;
+            //this.ofd1.Title = "Selecionar Fotos";
+            ////ofd1.InitialDirectory = @"C:\Users\macoratti\Pictures";
+            ////filtra para exibir somente arquivos de imagens
+            ////ofd1.Filter = "Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" + "All files (*.*)|*.*";
+            //ofd1.CheckFileExists = true;
+            //ofd1.CheckPathExists = true;
+            //ofd1.FilterIndex = 2;
+            //ofd1.RestoreDirectory = true;
+            //ofd1.ReadOnlyChecked = true;
+            //ofd1.ShowReadOnly = true;
+            
+            DialogResult dr = this.ofd1.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+
+                string arquivo;
+
+                arquivo = ofd1.FileName;
+
+                lblArquivo.Text = arquivo;
+                lblArquivo.Visible = true;
+
+                DataSet ds = new DataSet();
+                DataTable retorno = new DataTable();
+                ds.ReadXml(arquivo);
+
+                grdXml.DataSource = ds.Tables[0];
+
+                int cont = 0;
+
+                while (cont < grdXml.Columns.Count)
+                {
+                    if (!(grdXml.Columns[cont].HeaderText == "nome"))
+                        grdXml.Columns[cont].Visible = false;
+
+                    cont++;
+                }
+                
+                
+                // Le os arquivos selecionados 
+                //foreach (String arquivo in ofd1.FileNames)
+                //{
+                
+
+
+                //XDocument xDoc = XDocument.Parse(arquivo);
+
+                //var resultado = (from x in xDoc.Descendants()
+                //                 select new
+                //                 {
+                //                     Nome = x.Element("nome").Value,
+                //                     Idade = x.Element("telefone").Value
+                //                 }
+                //                ).ToList();
+
+
+
+                     
+
+                //    // cria um PictureBox
+                //    try
+                //    {
+                //        PictureBox pb = new PictureBox();
+                //        Image Imagem = Image.FromFile(arquivo);
+                //        pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                //        //para exibir as imagens em tamanho natural 
+                //        //descomente as linhas abaixo e comente as duas seguintes
+                //        //pb.Height = loadedImage.Height;
+                //        //pb.Width = loadedImage.Width;
+                //        pb.Height = 100;
+                //        pb.Width = 100;
+                //        //atribui a imagem ao PictureBox - pb
+                //        pb.Image = Imagem;
+                //        //inclui a imagem no containter flowLayoutPanel
+                //        flowLayoutPanel1.Controls.Add(pb);
+                //    }
+                //    catch (SecurityException ex)
+                //    {
+                //        // O usuário  não possui permissão para ler arquivos
+                //        MessageBox.Show("Erro de segurança Contate o administrador de segurança da rede.\n\n" +
+                //                                    "Mensagem : " + ex.Message + "\n\n" +
+                //                                    "Detalhes (enviar ao suporte):\n\n" + ex.StackTrace);
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        // Não pode carregar a imagem (problemas de permissão)
+                //        MessageBox.Show("Não é possível exibir a imagem : " + arquivo.Substring(file.LastIndexOf('\\'))
+                //                                   + ". Você pode não ter permissão para ler o arquivo , ou " +
+                //                                   " ele pode estar corrompido.\n\nErro reportado : " + ex.Message);
+                //    }
+                //}
+            //}
+        }//fim e evento botão
+        }
+
+        private void btnXml_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
